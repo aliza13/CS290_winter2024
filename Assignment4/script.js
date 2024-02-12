0fdfd5b53265620d504f9c72fc2fb6808d941525
aliza13 <alizaajc13@gmail.com>
2024-02-12 11:24:50 -0800

    Design and Basic JS

--- Assignment4/script.js
+++ Assignment4/script.js
@@ -1,4 +1,29 @@
+function greetingUser() {
+    var theName = document.getElementById('nameInput').value;
+    var outputDiv = document.getElementById("outputName");
+    var lettersAndSpacesOnly = /^[A-Za-z\s]+$/;
 
+    if (theName === '' || !theName.match(lettersAndSpacesOnly)) {
+        alert("Please enter a valid name containing only letters and spaces.");
+        return; // Exit the function early if validation fails
+    }
+    outputDiv.innerHTML = "<p>Hey there, " + theName + "!</p>";
+    console.log(theName);
+
+    // Disable button after name is submitted
+    document.getElementById("submitButton").disabled = true;
+    nameInput.disabled = true;
+
+    document.getElementById("levelInput").style.display = "block";
+}
+
+function storeLevels() {
+    var userInput = document.getElementById("userLevels").value.trim();
+    var levelsArray = [];
+    var levelsArray = userInput.split(/[,\s]+/).map(Number);
+    
+    console.log(levelsArray);
+}
 
 function showAlert() {
     alert("ALERT ALERT lol");
