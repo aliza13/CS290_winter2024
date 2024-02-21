22d272051f1c1d0bef6070ba044b533f2e0fc471
aliza13 <alizaajc13@gmail.com>
2024-02-21 10:54:59 -0800

    worked on objects

--- Assignment4/README.txt
+++ Assignment4/README.txt
@@ -4,4 +4,7 @@ Create a webpage with enough elements and styles that allow you to show off some
 
 Go find a JavaScript library that has some features that you think might be useful and be ready to discuss some of this with the class
 
+https://react.dev/ build UI's web apps
+
 Attempt to use that library to create another page that demonstrates some key features of the library (perhaps by going through a tutorial for that library?)
+- follow a course
--- Assignment4/script.js
+++ Assignment4/script.js
@@ -1,26 +1,44 @@
-function greetingUser() {
-    var theName = document.getElementById('nameInput').value;
+document.getElementById("submitButton").disabled = false; // no display right now
+
+let person_info = {
+    name: "",
+    levelsArray: [],
+    highs: [],
+    lows: [],
+    average: 0,
+    displayInfo: function() {
+        if (this.average == 0) {
+            console.log(`${this.name} your avg isn't available`);
+            return;
+        } else {
+            console.log(`${this.name} your average levels is ${this.average}`);}    }
+};
+
+function greetingUser(name) {
+    var name = document.getElementById('nameInput').value;
     var outputDiv = document.getElementById("outputName");
     var lettersAndSpacesOnly = /^[A-Za-z\s]+$/;
 
-    if (theName === '' || !theName.match(lettersAndSpacesOnly)) {
+    if (name === '' || !name.match(lettersAndSpacesOnly)) {
         alert("Please enter a valid name containing only letters and spaces.");
         return; // Exit the function early if validation fails
     }
-    outputDiv.innerHTML = "<p>Hey there, " + theName + "!</p>";
-    console.log(theName);
+    outputDiv.innerHTML = "<p>Hey there, " + name + "!</p>";
+    console.log(name);
 
     // Disable button after name is submitted
     document.getElementById("submitButton").disabled = true;
     nameInput.disabled = true;
+
+    person_info.name = name;
 
     document.getElementById("levelInput").style.display = "block";
 }
 
 function storeLevels() {
     var userInput = document.getElementById("userLevels").value.trim();
-    var levelsArray = [];
-    var levelsArray = userInput.split(/[,\s]+/).map(Number);
+    var levelsArray = []; // empty array to them store
+    var levelsArray = userInput.split(/[,\s]+/).map(Number); // split nums by , and space
     
     console.log(levelsArray);
 }
